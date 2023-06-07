7d385dd4191d52ca02112e229401f7b31b96035e
emilioff8822 <emilioff@hotmail.it>
2023-06-07 17:38:53 +0200

    commit-bonuspt2

--- modelcontroller/resources/views/details.blade.php
+++ modelcontroller/resources/views/details.blade.php
@@ -2,12 +2,10 @@
 @section('content')
     <div class="container">
         <h1>{{ $movie->title }}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laudantium a magni aliquam atque neque vero quos
-            inventore ab, accusamus fugit quisquam quis suscipit vitae sed unde nam animi et enim impedit possimus iusto
-            consequatur? Asperiores sequi earum maiores vero sunt reiciendis consequuntur possimus unde labore temporibus
-            voluptatibus placeat itaque similique debitis, laborum praesentium dignissimos voluptates illum soluta adipisci
-            ullam omnis iure deserunt. Ad accusantium, sed dolore tempore est quas vitae, aspernatur porro culpa dolor hic
-            quae unde ipsum deserunt, qui libero blanditiis quos. Similique recusandae dignissimos adipisci reiciendis nihil
-            quis, laborum nemo! Velit ducimus earum libero consequatur sint! Possimus!</p>
+        <p><strong>Titolo Originale</strong> {{ $movie->original_title }}</p>
+        <p><strong>Nazionalita</strong> {{ $movie->nationality }}</p>
+        <p><strong>Data</strong> {{ $movie->date }}</p>
+        <p><strong>Voto</strong> {{ $movie->vote }}</p>
+
     </div>
 @endsection
