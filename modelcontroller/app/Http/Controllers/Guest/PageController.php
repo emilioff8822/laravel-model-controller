752bb56a6a486798293690ae61b2d95c76fc0e48
emilioff8822 <emilioff@hotmail.it>
2023-06-07 18:17:19 +0200

    commit-compact

--- modelcontroller/app/Http/Controllers/Guest/PageController.php
+++ modelcontroller/app/Http/Controllers/Guest/PageController.php
@@ -8,55 +8,27 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-
-public function index () {
+    public function index () {
         $title = 'home';
         return view('home', compact('title'));
-}
+    }
 
+    public function movies () {
+        // $movies = movies::all(); // SELECT * ALL  from movies
+        $movies = movies::orderBy('title')->get(); //ordina i film in ordine alfabetico per titolo
+        return view('movies', compact('movies'));
+    }
 
-public function movies () {
-
-// $movies = movies::all(); // SELECT * ALL  from movies
-$movies = movies::orderBy('title')->get(); //ordina i film in ordine alfabetico per titolo
-
-// select con film  con id=2 restituisce direttament l'ogetto
-//$movie = movies::find(2);
-
-//select con id=2 ma con where che PUSHA i risultati in un array. 2 param 1 colonna 2 valore. TRE param 1 colonna 2operatore 3valore
-//$movie = movies::where('id', 2)->get();
-
-//uso first
-//$movie = movies::where('id', 1)->first();
-
-return view('movies', compact('movies'));
-
-}
-
-public function contacts () {
+    public function contacts () {
         return view('contacts');
+    }
 
-}
-
-public function details ($id) {
-        $movie = movies::find($id);
-
-        if($movie){
-            return view('details', ['movie' => $movie]); //uso un array associativo per passare un array alla vista detail e poi stamparlo 'movie' e' la chiave , $movie e' il valore. 'movie' lo riutilizzo in details.se cambiassi 'movie' a 'film' nel controller, allora dovrei anche cambiare tutte le istanze di $movie nella tua vista a $film.
+    public function details ($id) {
+    //$movie = movies::where('id', $id)->first();
 
 
 
-
-
-
-        }else {
-            return redirect()->route ('movies');
-        }
-
-}
-
-
-
-
-
+    $movie = movies::find($id);
+    return view('details', compact('movie'));
+    }
 }
